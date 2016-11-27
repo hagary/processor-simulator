290bd90f222ae3d7754670a429d34f0159f37bf0
Rania Wael <rania.waelsameh.rw@gmail.com>
2016-11-27 13:02:37 +0200

    Register File
    array of 8 registers

--- src/registers/RegisterFile.java
+++ src/registers/RegisterFile.java
@@ -1,5 +1,23 @@
 package registers;
 
 public class RegisterFile {
+	private Register [] registers;
+	
+	public RegisterFile(){
+		registers = new Register[8];
+		
+		for(int i = 0; i < registers.length; i++){
+			registers[i] = new Register();
+		}
+	}
 
+	public Register[] getRegisters() {
+		return registers;
+	}
+
+	public void setRegisters(Register[] registers) {
+		this.registers = registers;
+	}
+	
+	
 }
