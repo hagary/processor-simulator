29b023751716f0706fbbfa7a8e2c56d810d372d3
Mariam Mazen <mariammazen@paymobsolutions.com>
2016-11-27 20:24:42 +0200

    rana's changes

--- src/instructions/types/Load.java
+++ src/instructions/types/Load.java
@@ -1,7 +1,19 @@
 package instructions.types;
 
 import instructions.Instruction;
+import tomasulo.InstructionParameters;
+import simulator.Simulator;
+import memory.Word;
 
 public class Load extends Instruction {
-
+	
+	public short execute(InstructionParameters IP){
+		short regB = IP.getRegB();
+		short imm = IP.getImm();
+		short address = (short) (regB+ imm);	
+		String result = (Simulator.getDataMem().readWord(address)).getData(); //convert to short
+		Short s = Short.parseShort(result);
+		return 0; //result
+	}
+	
 }
--- src/instructions/types/Nand.java
+++ src/instructions/types/Nand.java
@@ -1,7 +1,13 @@
 package instructions.types;
 
 import instructions.Instruction;
+import tomasulo.InstructionParameters;
 
 public class Nand extends Instruction{
-
+	
+	public short execute(InstructionParameters IP){
+		short regB = IP.getRegB();
+		short regC = IP.getRegC();
+		return (short)~(regB & regC);
+	}
 }
