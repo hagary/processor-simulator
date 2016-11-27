59f19b5f4bada0115561e5f1a089c8e163f553a4
Rania Wael <rania.waelsameh.rw@gmail.com>
2016-11-27 19:52:30 +0200

    ROB Again

--- src/tomasulo/ROB.java
+++ src/tomasulo/ROB.java
@@ -1,5 +1,21 @@
 package tomasulo;
-
-public class ROB {
-
-}
+  
+ import java.util.LinkedList;
+ import java.util.Queue;
+ 
+  public class ROB {
+ 	private Queue<ROBEntry> ROBTable;
+ 
+ 	public ROB()
+	{
+ 		ROBTable = new LinkedList<ROBEntry>();
+ 	}
+ 	public Queue<ROBEntry> getROBTable() {
+ 		return ROBTable;
+ 	}
+  
+ 	public void setROBTable(Queue<ROBEntry> rOBTable) {
+ 		ROBTable = rOBTable;
+ 	}
+ 	
+  }
