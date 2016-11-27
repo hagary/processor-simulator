4d236e078d372609c0729a15e56da0dd2970cdd5
Hagar Mosaad <hagary@users.noreply.github.com>
2016-11-27 16:14:49 +0200

    Revert "Skeleton of ROB and RS"

--- src/tomasulo/RSSet.java
+++ src/tomasulo/RSSet.java
@@ -1,18 +1,5 @@
 package tomasulo;
 
 public class RSSet {
-	private RS[] RSArray;
 
-	public RSSet()
-	{
-		//TODO how are we going to declare the things
-	}
-	public RS[] getRSarray() {
-		return RSArray;
-	}
-
-	public void setRSarray(RS[] rSarray) {
-		RSArray = rSarray;
-	}
-	
 }
