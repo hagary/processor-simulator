d061a8422e0da99f5dfbbbea7496cd461c11f5d4
Rania Wael <rania.waelsameh.rw@gmail.com>
2016-11-27 21:37:58 +0200

    #38 Registers File

--- src/registers/Register.java
+++ src/registers/Register.java
@@ -1,5 +1,18 @@
 package registers;
 
 public class Register {
+	private short data;
 	
+	public Register(){
+		data = 0;
+	}
+
+	public short getData() {
+		return data;
+	}
+
+	public void setData(short data) {
+		this.data = data;
+	}
+
 }
